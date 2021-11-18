cff2ad7eb3ce6a1a543dec2382efef9380204109
Vaibhav Dhawan <vaibhav.dhawan@dominodatalab.com>
2021-11-18 10:00:48 -0800

    Pushed from Domino: /u/vaibhav_dhawan/java_spring/runs/619656829e51864ae17e2ab7

--- app.sh
+++ app.sh
@@ -2,4 +2,5 @@ sed -i "s/USER_NAME/$DOMINO_PROJECT_OWNER/g" /mnt/code/complete/src/main/resourc
 sed -i "s/PROJECT_NAME/$DOMINO_PROJECT_NAME/g" /mnt/code/complete/src/main/resources/application.properties
 sed -i "s/RUN_ID/$DOMINO_RUN_ID/g" /mnt/code/complete/src/main/resources/application.properties
 
-sh /mnt/code/complete/mvnw spring-boot:run
+cd /mnt/code/complete/
+./mvnw spring-boot:run
